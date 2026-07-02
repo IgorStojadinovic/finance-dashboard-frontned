fc22dc37ddb48d805a37f7c24aee5972e4fc7c7d
IgorST <igorstojadinovic.vs@gmail.com>
2026-07-02 18:50:20 +0200

    config updates

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,36 @@
 /// <reference types="vitest" />
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    test: {
-        environment: 'jsdom',
-        globals: true,
-        setupFiles: ['./tests/setup.ts'],
-        include: ['**/*.{test,spec}.{js,jsx,ts,tsx}'],
-        exclude: ['node_modules', 'dist', '.idea', '.git', '.cache'],
-        testTransformMode: {
-            web: ['.[jt]sx'],
-        },
-        deps: {
-            inline: ['@headlessui/react'],
-        },
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./tests/setup.ts'],
+    include: ['**/*.{test,spec}.{js,jsx,ts,tsx}'],
+    exclude: ['node_modules', 'dist', '.idea', '.git', '.cache'],
+    testTransformMode: {
+      web: ['.[jt]sx'],
     },
-    resolve: {
-        extensions: ['.js', '.ts', '.jsx', '.tsx'],
+    deps: {
+      inline: ['@headlessui/react'],
     },
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:3000',
-                changeOrigin: true,
-            },
-        },
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx'],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
+  },
 });
